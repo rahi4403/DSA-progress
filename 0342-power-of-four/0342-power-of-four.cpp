3b8207094f48d6cb85f486048f0ca26c4f6de0fb
Ishika Pandey <rahi7102005@gmail.com>
2026-07-06 14:58:08 +0530

    Time: 0 ms (100%), Space: 8 MB (2.54%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        for(int i=0;i<=15;i++){
+        for(int i=0;i<=15;i++){ //because a power of 4 grows much faster so if we calculate 4 to the power 30 or beyond 15 it exceeds safe integer limit which is why our code breaks//
         int power=pow(4,i);
         if(n==power){
             return true;
