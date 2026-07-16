adaa427a8c70f6903fc53ba5267857496a5db7bf
Ishika Pandey <rahi7102005@gmail.com>
2026-07-16 15:02:50 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int i=0; //for the 1st index
         for(int j=0;j<nums.size();j++){ //for element to element traversing and checking 
-            if(nums[j]!=0){
+            if(nums[j]!=0){ //if it is already equal to 0 then ignore it,if not then swap it with the indexed position so 0 automatically moves after the non zero element
                 swap(nums[j],nums[i]);
            i++; //moves to next index to check
             }
