439690e08cc8394c8cbbaaa068fb28e09acaf242
Ishika Pandey <rahi7102005@gmail.com>
2026-07-21 14:27:09 +0530

    Time: 203 ms (36.81%), Space: 12.6 MB (91.75%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution { //Sieve of Eratosthenes method 
 public:
     int countPrimes(int n) {
         int count=0;
-        vector<bool> prime(n+1,true);
-        prime[0]=prime[1]=false;
-        for(int i=2;i<n;i++){
+        vector<bool> prime(n+1,true); //this is to create a boolean array,and we assume that every number is prime,hence we place true at the beginning
+        prime[0]=prime[1]=false; //since 0 and 1 are not prime we omit them at the very beginning
+        for(int i=2;i<n;i++){ //0 & 1 have been omitted already so we start the loop from 2
             if(prime[i]){
                 count++;
-                for(int j=2*i;j<n;j+=i)
-                prime[j]=0;
+                for(int j=2*i;j<n;j+=i) //this loop visits all the multiples of the number which has been called prime in the if(prime[i]),the entire loop checks for the multiples since the multiples will never be prime right?
+                prime[j]=0;//which is why we call them not prime(0-false)
                 }
         }
         return count;
