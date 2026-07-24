10d30c82db17b72cffe1d08de3aa26902d3ef682
Ishika Pandey <rahi7102005@gmail.com>
2026-07-24 19:56:33 +0530

    Time: 0 ms (100%), Space: 16.9 MB (60.21%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans(2*n);
-        for(int i=0;i<nums.size();i++){
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i]; //this is for the first half
+            ans[i+n]=nums[i]; //this is for the second half to access the next half and store the same elements (nums[i])
         }
         return ans;
         
