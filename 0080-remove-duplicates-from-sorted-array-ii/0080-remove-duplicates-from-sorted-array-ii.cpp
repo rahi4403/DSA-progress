82b0d9e5699db8cbba78b06e1698303c8744d05f
Ishika Pandey <rahi7102005@gmail.com>
2026-08-04 00:16:32 +0530

    Time: 7 ms (72.57%), Space: 19.6 MB (24.46%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int k=2; //because 1st 2 elements are already occupied
         int n=nums.size();
-        if(n<2){ //if the array has only 2 or less than that element then we return the size of the array itself,it has to be checked before the loop since otherwise it directly returns 2 from the for loop
+        if(n<=2){ //if the array has only 2 or less than that element then we return the size of the array itself,it has to be checked before the loop since otherwise it directly returns 2 from the for loop
                 return n;
             }
         for(int i=2;i<n;i++){
