c1b932ef5ba57c79db4381880fa558181ed90af7
Ishika Pandey <rahi7102005@gmail.com>
2026-08-20 20:43:29 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.66%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,6 +1,6 @@
 class Solution {
     private:
-    int getLength(ListNode* head){
+    int getLength(ListNode* head){ //this function is for traversing the entire length of the linked list to check for all the elements
         int len=0;
         while(head!=NULL){
             len++;
@@ -11,13 +11,13 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         int l=getLength(head);
-        int ans=(l)/2;
-        ListNode* temp=head;
+        int ans=(l)/2; //to get the exact middle node of the linked list 
+        ListNode* temp=head; //stores so that the original copy remains unchanged
         int count=0;
         while(count<ans){
-            temp=temp->next;
+            temp=temp->next; //checks for the middle element and increases the count 
             count++;
         }
-        return temp;
+        return temp;//then returns the exact node 
     }
 };
