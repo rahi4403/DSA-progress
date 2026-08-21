05acdcf95d6b8a77efc95a610a715ca10ba40946
Ishika Pandey <rahi7102005@gmail.com>
2026-08-21 21:34:33 +0530

    Time: 0 ms (100%), Space: 16.4 MB (96.92%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -4,13 +4,13 @@ public:
         if(head==NULL){
             return NULL;
         }
-        ListNode* temp=head;
+        ListNode* temp=head; //to check if there are at least k nodes,this creates a temporary pointer
         int count=0;
         while(temp!=NULL && count<k){
-            temp=temp->next;
+            temp=temp->next;//moves to the next node 
             count++;
         }
-        if(count<k){
+        if(count<k){ //when there are fewer nodes than the count
             return head;
         }
         ListNode* next=NULL;
@@ -19,12 +19,12 @@ public:
         count=0;
         while(curr!=NULL && count<k){
             next=curr->next;
-            curr->next=prev;
+            curr->next=prev;//this part does the reversal
             prev=curr;
-            curr=next;
+            curr=next;//moves to the next node
             count++;
         } 
-        if(next!=NULL){
+        if(next!=NULL){ //recursive call for other sections of the LL
             head->next=reverseKGroup(next,k);
         }
         return prev;
