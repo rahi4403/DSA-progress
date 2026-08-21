625bd563fdf03f57a7d0e0293aa626d8952b9d47
Ishika Pandey <rahi7102005@gmail.com>
2026-08-21 22:07:32 +0530

    Time: 8 ms (81.85%), Space: 11.9 MB (22.97%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL){ //empty list 
+        return false;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+            return false;
+        }
+    };
+    /*{
+        if(head==NULL){
+            return NULL;
+        }
+        ListNode* temp=head->next;
+        while(temp!=NULL && temp!=head){ //TLE because this runs forever,but the circle can start from somewhere in the middle too
+            temp=temp->next;
+        }
+        if(temp==head){
+            return true;
+        }
+        return false;
+    }
+};*/
