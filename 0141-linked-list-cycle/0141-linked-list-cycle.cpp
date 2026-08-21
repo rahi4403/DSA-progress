3026b5b023a0fa32d292116d14394bc750beed5e
Ishika Pandey <rahi7102005@gmail.com>
2026-08-21 23:20:32 +0530

    Time: 12 ms (41.72%), Space: 11.9 MB (54.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -4,12 +4,12 @@ public:
         if(head==NULL){ //empty list 
         return NULL;
         }
-        ListNode* slow=head;
+        ListNode* slow=head; //initialise 2 pointers to the same position(head)
         ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
+        while(fast!=NULL && fast->next!=NULL){ //checks if the fast has a next node and till it reaches the end of the list
+            slow=slow->next; //slow moves to the next node
+            fast=fast->next->next;//fast moves 2 nodes forward
+            if(slow==fast){ //checks for loops,i.e,same nodes at two points so it ends up becoming a circularly linked list
                 return true;
             }
         }
