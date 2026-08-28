59987448d86b071896b622a90e194e55f2c1829e
Ishika Pandey <rahi7102005@gmail.com>
2026-08-28 16:35:51 +0530

    Time: 11 ms (15.04%), Space: 15 MB (87.12%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,4 +1,62 @@
 class Solution {
+private:
+    void InsertAtTail(Node* &head, Node* &tail, int d) {
+        Node* newNode = new Node(d);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+public:
+    Node* copyRandomList(Node* head) {
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        Node* temp = head;
+        if(head==NULL){
+            return NULL;
+        }
+        while (temp != NULL) {
+            InsertAtTail(cloneHead, cloneTail, temp->val);
+            temp = temp->next;
+        }
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+        while (originalNode != NULL && cloneNode != NULL) {
+            Node* next = originalNode->next;
+            originalNode->next = cloneNode;
+            originalNode = next;
+            next = cloneNode->next;
+            cloneNode->next = originalNode;
+            cloneNode = next;
+        }
+        temp = head;
+        while (temp != NULL) {
+            if (temp->next != NULL) {
+                temp->next->random =
+                    temp->random ? temp->random->next : NULL;
+            }
+            temp = temp->next->next;
+        }
+        originalNode = head;
+        cloneNode = head->next;
+        while (originalNode != NULL) {
+            originalNode->next = cloneNode->next;
+            originalNode = originalNode->next;
+            if (originalNode != NULL) {
+                cloneNode->next = originalNode->next;
+            }
+            cloneNode = cloneNode->next;
+        }
+        return cloneHead;
+    }
+};
+        
+        /*class Solution {
     private:
     void InsertAtTail(Node* &head,Node* &tail,int d){
         Node* newNode=new Node(d);
@@ -38,4 +96,4 @@ public:
         }
         return cloneHead;
     }
-};
+};*/
