a1a90408368a8a004508e5657e382de482773bb9
Ishika Pandey <rahi7102005@gmail.com>
2026-09-02 00:54:51 +0530

    Time: 0 ms (100%), Space: 15.8 MB (40.81%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-       ListNode* curr=head;
+       ListNode* curr=head;//curr points to head 
        ListNode* prev=NULL;
-       while(curr!=NULL){
-       if(curr->next!=NULL && curr->val==curr->next->val){
-        while(curr->next!=NULL && curr->val==curr->next->val){
-            curr=curr->next;
+       while(curr!=NULL){ //till it reaches the end of the list
+       if(curr->next!=NULL && curr->val==curr->next->val){ //checks if the current node has a next node and if the duplicate is present
+        while(curr->next!=NULL && curr->val==curr->next->val){ //same condition to keep moving the next node until these conditions come
+            curr=curr->next; //keep moving curr till any of the while conditions come
         }
-        if(prev==NULL){
-            head=curr->next;
-        }
-        else{
-            prev->next=curr->next;
-        }
-        curr=curr->next;
+        if(prev==NULL){ //to check if there are no nodes before the duplicates begin
+            head=curr->next; //makes head point to the node after the duplicate group
         }
         else{
-            prev=curr;
+            prev->next=curr->next; //when duplicate group is not at beginning,deletes the duplicate group by changing the nodes after prev(prev->nodes after duplicates instead of prev->duplicate nodes)
+        }
+        curr=curr->next; //first node after the duplicate groups,since we need to omit the duplicate group
+        }
+        else{
+            prev=curr; //updation of prev
             curr=curr->next;
         }
        }
-        return head;
+        return head; //returns the modified list
        }
     };
