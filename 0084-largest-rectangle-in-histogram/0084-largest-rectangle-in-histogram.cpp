2c787baa4062ec8a969c35e49b2aa19c5c79cd08
Ishika Pandey <rahi7102005@gmail.com>
2026-09-16 23:35:15 +0530

    Time: 43 ms (8.82%), Space: 98.1 MB (5.02%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,15 +2,15 @@ class Solution {
     private:
 vector<int> nextSmallerElement(vector<int> arr,int n){
     stack<int> s;
-    s.push(-1);
+    s.push(-1); //dummy index for when there is no smaller index
     vector<int> ans(n);
-    for(int i=n-1;i>=0;i--){
-        int curr=arr[i];
-        while(s.top()!=-1 && arr[s.top()]>=curr){
+    for(int i=n-1;i>=0;i--){ //because we are looking for smaller element to the right 
+        int curr=arr[i]; //stores current element in curr
+        while(s.top()!=-1 && arr[s.top()]>=curr){ //we remove elements from the stack till it reaches -1 and height of the stack is greater than or equal to the curr element
             s.pop();
         }
-        ans[i]=s.top();
-        s.push(i);
+        ans[i]=s.top(); //index of the next smaller element
+        s.push(i);//that is pushed
     }
     return ans;
 }
@@ -19,7 +19,7 @@ vector<int> prevSmallerElement(vector<int> arr,int n){
     stack<int> s;
     s.push(-1);
     vector<int> ans(n);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){ //checking for smaller element to the left
         int curr=arr[i];
         while(s.top()!=-1 && arr[s.top()]>=curr){
             s.pop();
@@ -33,18 +33,18 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
         vector<int> next(n);
-        next=nextSmallerElement(heights,n);
+        next=nextSmallerElement(heights,n); //calls the function and stores element in next
         vector<int> prev(n);
-        prev=prevSmallerElement(heights,n);
-        int area=INT_MIN;
+        prev=prevSmallerElement(heights,n);//calls function to store previous element
+        int area=INT_MIN; //to store maximum area
         for(int i=0;i<n;i++){
-            int l=heights[i];
+            int l=heights[i]; //stores height of current bar in l 
             if(next[i]==-1){
-                next[i]=n;
+                next[i]=n; //if no smaller element found we state n as boundary
             }
-                        int b=next[i]-prev[i]-1;
+                        int b=next[i]-prev[i]-1; //calculates the width of the largest rectangle
             int newArea=l*b;
-            area=max(area,newArea);
+            area=max(area,newArea); //compares then returns the actual answer
         }
         return area;
     }
