b5765c4300444e4ebf84bca22e89b4172ca5bb79
Ishika Pandey <rahi7102005@gmail.com>
2026-09-23 13:07:00 +0530

    Time: 3 ms (74.62%), Space: 28.4 MB (62.44%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,15 +3,14 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         stack<int> s;
-        vector<int> ans(n);
-        for(int i=2*n-1;i>=0;i--){
-            int index=i%n;
-            while(!s.empty() && s.top()<=nums[index]){
+        vector<int> ans(n); //creating an answer with n positions
+        for(int i=2*n-1;i>=0;i--){ //since it is a circular array 
+            int index=i%n; //to access the actual array index since we are pretending the element is appearing twice
+            while(!s.empty() && s.top()<=nums[index]){//similar to next greater element I
                 s.pop();
             }
                 if(s.empty()){
-
-ans[index]=-1;
+                ans[index]=-1; //if no greater element is available,store -1(dummy index)
                 }
                 else{
                     ans[index]=s.top();
