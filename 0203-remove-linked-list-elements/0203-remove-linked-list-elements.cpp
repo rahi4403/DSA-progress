ec829ce3eddc5d43139cfbea528b28e99f5aa209
Ishika Pandey <rahi7102005@gmail.com>
2026-09-25 19:48:00 +0530

    Time: 0 ms (100%), Space: 20.1 MB (42.52%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        while(head!= NULL && head->val==val){
-            head=head->next;
+        while(head!= NULL && head->val==val){ //list is not empty and the current head contains value to remove
+            head=head->next; //move head pointer to the next node
         }
         ListNode* curr=head;
-        while(curr!=NULL && curr->next!=NULL){
-            if(curr->next->val==val){
-                curr->next=curr->next->next;
+        while(curr!=NULL && curr->next!=NULL){ //to check if curr exists and the next node after curr exists
+            if(curr->next->val==val){ //checks if the next node matches the value
+                curr->next=curr->next->next;//skips the actual value to be removed,by connecting the next of curr to next-to-next of curr
             }
             else{
-                curr=curr->next;
+                curr=curr->next;//update curr
             }
         }
         return head;
